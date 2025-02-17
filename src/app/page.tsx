0591ee6a6aac26502fdfd66ce7fbc7f062706658
Matthews, Quinn <Quinn.Matthews@lrs.com>
2025-02-17 17:24:36 -0600

    fix eslint issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
-        <img src="./BYXxSMU.png" className="w-[15rem]"></img>
+        <img src="./BYXxSMU.png" className="w-[15rem]" alt=""></img>
         <h1 className="text-3xl">Submit Feedback</h1>
         <form action={formAction}>
       <p>What is our abbreviated secret phrase?*</p>
