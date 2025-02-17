8b788465be005c1b7678b1108d2a8701d4f81dd7
Matthews, Quinn <Quinn.Matthews@lrs.com>
2025-02-17 17:20:55 -0600

    Style updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ function SubmitButton() {
     <button
       type="submit"
       aria-disabled={pending}
-      className="mt-2 rounded border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+      className="mt-2 rounded border border-solid border-transparent transition-colors flex items-center justify-center bg-[#4F2D7F] text-white gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
     >
       Submit
     </button>
@@ -30,75 +30,22 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+        <img src="./BYXxSMU.png" className="w-[15rem]"></img>
+        <h1 className="text-3xl">Submit Feedback</h1>
         <form action={formAction}>
       <p>What is our abbreviated secret phrase?*</p>
-      <input type="text" id="secretPhrase" name="secretPhrase" required className="rounded w-[100%] dark:bg-slate-600 p-2" />
+      <input type="text" id="secretPhrase" name="secretPhrase" required className="rounded w-[100%] border border-black dark:bg-slate-600 p-2" />
       <p className="mt-1">Feedback*</p>
-      <textarea id="feedback" name="feedback" required className="rounded w-[100%] dark:bg-slate-600 p-2" rows={4}></textarea>
+      <textarea id="feedback" name="feedback" required className="rounded w-[100%] border border-black dark:bg-slate-600 p-2" rows={4}></textarea>
       <p>Name (Optional)</p>
-      <input type="text" id="name" name="name" className="rounded w-[100%] dark:bg-slate-600 p-2" />
+      <input type="text" id="name" name="name" className="rounded w-[100%] border border-black dark:bg-slate-600 p-2" />
       <SubmitButton />
       <p role="status">
         {state?.message}
       </p>
     </form>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
